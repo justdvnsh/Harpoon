c8de1aab6947b753bf41c5f7225fe05d9554f4f9
Kira Gami <justdvnsh2208@gmail.com>
2020-12-24 01:03:37 +0530

    added dependencies and model structure

--- app/src/main/java/divyansh/tech/harpoon/views/activities/MainActivity.kt
+++ app/src/main/java/divyansh/tech/harpoon/views/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package divyansh.tech.harpoon
+package divyansh.tech.harpoon.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import divyansh.tech.harpoon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
